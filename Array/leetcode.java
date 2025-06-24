b7e419435a7cdaf2fe97c71d3608adf5c612766a
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-06-24 11:37:08 +0530

    push 9

--- Array/leetcode.java
+++ Array/leetcode.java
@@ -5,61 +5,102 @@
 // }
 
 
-1752. Check if Array Is Sorted and Rotated
+// 1752. Check if Array Is Sorted and Rotated
 
 
-class Solution {
-    public boolean check(int[] nums) {
-        int count = 0;
-        int n = nums.length;
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int count = 0;
+//         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
-                count++;
-            }
-        }
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] > nums[(i + 1) % n]) {
+//                 count++;
+//             }
+//         }
 
-        return count <= 1;
-    }
-}
-
-
-
-26. Remove Duplicates from Sorted Array
-
-
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int i = 1, j = 1;
-       while(j < nums.length){
-        if(nums[j] == nums[j-1])j++;
-        else {
-            nums[i] = nums[j];
-            i++;
-            j++;
-        }
-       }
-       return i;
-    }
-}
+//         return count <= 1;
+//     }
+// }
 
 
 
+// 26. Remove Duplicates from Sorted Array
 
-1281. Subtract the Product and Sum of Digits of an Integer
 
-class Solution {
-    public int subtractProductAndSum(int n) {
-        int sum=0;
-        int pro=1;
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int i = 1, j = 1;
+//        while(j < nums.length){
+//         if(nums[j] == nums[j-1])j++;
+//         else {
+//             nums[i] = nums[j];
+//             i++;
+//             j++;
+//         }
+//        }
+//        return i;
+//     }
+// }
 
-        while(n>0){
-            int digit=n%10;
-            sum+=digit;
-            pro=pro*digit;
-            n=n/10;
-        }
-        int ans=pro-sum;
-        return ans;
-    }
-}
+
+
+
+// 1281. Subtract the Product and Sum of Digits of an Integer
+
+// class Solution {
+//     public int subtractProductAndSum(int n) {
+//         int sum=0;
+//         int pro=1;
+
+//         while(n>0){
+//             int digit=n%10;
+//             sum+=digit;
+//             pro=pro*digit;
+//             n=n/10;
+//         }
+//         int ans=pro-sum;
+//         return ans;
+//     }
+// }
+
+
+
+
+// 191. Number of 1 Bits
+
+// class Solution {
+//     public int hammingWeight(int n) {
+//         int count=0;
+//         while(n>0){
+//             int ldigit=n%2;
+//             if(ldigit==1) count++;
+//             n=n/2;
+//         }
+//         return count;
+//     }
+// }
+
+
+
+// 704. Binary Search
+
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int slow=0;
+//         int fast=nums.length-1;
+//         while(slow<=fast){
+//         int mid=(slow+fast)/2;
+//         if(target==nums[mid]) return mid;
+//         else if(target<nums[mid]){
+//             fast=mid-1;
+//         }
+//         else{
+//             slow=mid+1;
+//         }
+//         }
+//         return -1;
+        
+//     }
+// }
--- linkedlist/leetcode .java
+++ linkedlist/leetcode .java
@@ -1,23 +1,24 @@
 
-83. Remove Duplicates from Sorted List
 
-class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head==null) return null;
-       ListNode slow=head;
-       ListNode fast=head.next;
+// 83. Remove Duplicates from Sorted List
 
-       while(fast!=null){
-        if(slow.val==fast.val){
-            slow.next=fast.next;
-        }
-        else{
-            slow=slow.next;
-        }
-        fast=fast.next;
-       }
-       return head;
-    }
-}
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if(head==null) return null;
+//        ListNode slow=head;
+//        ListNode fast=head.next;
+
+//        while(fast!=null){
+//         if(slow.val==fast.val){
+//             slow.next=fast.next;
+//         }
+//         else{
+//             slow=slow.next;
+//         }
+//         fast=fast.next;
+//        }
+//        return head;
+//     }
+// }
 
 
