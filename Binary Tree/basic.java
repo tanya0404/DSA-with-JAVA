7629ac7bff6f42fb4c7c482c2d8ccb7ff7a58bee
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-07-15 11:52:12 +0530

    push 15

--- Binary Tree/basic.java
+++ Binary Tree/basic.java
@@ -1,4 +1,44 @@
 public class basic {
+// public static class node{
+//     int data;
+//     node left;
+//     node right;
+
+//     node(int data){
+//         this.data=data;
+//     }
+// }
+
+//     public static void display(node root){
+//         if(root==null) return;
+//         System.out.print(root.data+" ");
+//         display(root.left);
+//         display(root.right);
+//     }
+
+//     public static void main(String[] args) {
+//         node a=new node(10);
+//         node b=new node(30);
+//         node c=new node(20);
+//         node d=new node(40);
+//         node e=new node(70);
+//         node f=new node(90);
+
+//         a.left=b;a.right=c;
+//         b.left=d;b.right=e;
+//         c.right=f;
+
+//         display(a);
+
+// }
+
+
+
+
+
+
+
+
 public static class node{
     int data;
     node left;
@@ -9,11 +49,11 @@ public static class node{
     }
 }
 
-    public static void display(node root){
-        if(root==null) return;
-        System.out.print(root.data+" ");
-        display(root.left);
-        display(root.right);
+    public static int displaysum(node root){
+        int sum =0;
+        if(root==null) return 0;
+        sum = root.data+displaysum(root.left)+displaysum(root.right);
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -28,7 +68,7 @@ public static class node{
         b.left=d;b.right=e;
         c.right=f;
 
-        display(a);
+        System.out.println(displaysum(a));
 
 }
 
