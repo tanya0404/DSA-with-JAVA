b074f8377553e0a967c25589a46f3b81c2f8d909
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-07-15 12:23:29 +0530

    push 16

--- Binary Tree/basic.java
+++ Binary Tree/basic.java
@@ -39,6 +39,45 @@ public class basic {
 
 
 
+// public static class node{
+//     int data;
+//     node left;
+//     node right;
+
+//     node(int data){
+//         this.data=data;
+//     }
+// }
+
+//     public static int displaysum(node root){
+//         int sum =0;
+//         if(root==null) return 0;
+//         sum = root.data+displaysum(root.left)+displaysum(root.right);
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         node a=new node(10);
+//         node b=new node(30);
+//         node c=new node(20);
+//         node d=new node(40);
+//         node e=new node(70);
+//         node f=new node(90);
+
+//         a.left=b;a.right=c;
+//         b.left=d;b.right=e;
+//         c.right=f;
+
+//         System.out.println(displaysum(a));
+
+// }
+
+
+
+
+
+
+
 public static class node{
     int data;
     node left;
@@ -49,27 +88,32 @@ public static class node{
     }
 }
 
-    public static int displaysum(node root){
-        int sum =0;
-        if(root==null) return 0;
-        sum = root.data+displaysum(root.left)+displaysum(root.right);
-        return sum;
+    public static int displayproduct(node root){
+        int product=1;
+        if(root==null) return 1;
+        if(root.data!=0){
+            product=root.data*displayproduct(root.left)*displayproduct(root.right);
+        }else{
+            product=displayproduct(root.left)*displayproduct(root.right);
+        }
+        return product;
     }
 
     public static void main(String[] args) {
-        node a=new node(10);
-        node b=new node(30);
-        node c=new node(20);
-        node d=new node(40);
-        node e=new node(70);
-        node f=new node(90);
+        node a=new node(1);
+        node b=new node(0);
+        node c=new node(2);
+        node d=new node(4);
+        node e=new node(7);
+        node f=new node(9);
 
         a.left=b;a.right=c;
         b.left=d;b.right=e;
         c.right=f;
 
-        System.out.println(displaysum(a));
+        System.out.println(displayproduct(a));
 
 }
+
 
 }
