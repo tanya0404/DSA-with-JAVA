b5b75f5217e5b2429e3d33a587a990e27bc37d1f
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-07-15 13:07:10 +0530

    push 17

--- Binary Tree/basic.java
+++ Binary Tree/basic.java
@@ -78,6 +78,93 @@ public class basic {
 
 
 
+// public static class node{
+//     int data;
+//     node left;
+//     node right;
+
+//     node(int data){
+//         this.data=data;
+//     }
+// }
+
+//     public static int displayproduct(node root){
+//         int product=1;
+//         if(root==null) return 1;
+//         if(root.data!=0){
+//             product=root.data*displayproduct(root.left)*displayproduct(root.right);
+//         }else{
+//             product=displayproduct(root.left)*displayproduct(root.right);
+//         }
+//         return product;
+//     }
+
+//     public static void main(String[] args) {
+//         node a=new node(1);
+//         node b=new node(0);
+//         node c=new node(2);
+//         node d=new node(4);
+//         node e=new node(7);
+//         node f=new node(9);
+
+//         a.left=b;a.right=c;
+//         b.left=d;b.right=e;
+//         c.right=f;
+
+//         System.out.println(displayproduct(a));
+
+// }
+
+
+
+
+
+
+// public static class node{
+//     int data;
+//     node left;
+//     node right;
+
+//     node(int data){
+//         this.data=data;
+//     }
+// }
+
+//     static int max=Integer.MIN_VALUE;
+//     public static void displaymax(node root){
+//         if(root==null) return;
+//         if(root.data>max) max=root.data;
+//         displaymax(root.left);
+//         displaymax(root.right);
+
+//     }
+
+//     public static void main(String[] args) {
+//         node a=new node(1);
+//         node b=new node(3);
+//         node c=new node(2);
+//         node d=new node(4);
+//         node e=new node(7);
+//         node f=new node(9);
+
+//         a.left=b;a.right=c;
+//         b.left=d;b.right=e;
+//         c.right=f;
+
+//         displaymax(a);
+//         System.out.println(max);
+
+// }
+
+
+
+
+
+
+
+
+
+
 public static class node{
     int data;
     node left;
@@ -88,20 +175,16 @@ public static class node{
     }
 }
 
-    public static int displayproduct(node root){
-        int product=1;
-        if(root==null) return 1;
-        if(root.data!=0){
-            product=root.data*displayproduct(root.left)*displayproduct(root.right);
-        }else{
-            product=displayproduct(root.left)*displayproduct(root.right);
-        }
-        return product;
+public static int displaysize(node root){
+        
+        if (root==null) return 0;
+        return 1+displaysize(root.left)+displaysize(root.right);
+
     }
 
     public static void main(String[] args) {
         node a=new node(1);
-        node b=new node(0);
+        node b=new node(3);
         node c=new node(2);
         node d=new node(4);
         node e=new node(7);
@@ -111,9 +194,12 @@ public static class node{
         b.left=d;b.right=e;
         c.right=f;
 
-        System.out.println(displayproduct(a));
+        System.out.println(displaysize(a));
+        
 
 }
+
+
 
 
 }
