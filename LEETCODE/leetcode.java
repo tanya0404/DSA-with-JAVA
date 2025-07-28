c30d6fdcf15433ade0adef4bbce8b199de794200
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-07-28 23:38:10 +0530

    1 of 90

--- LEETCODE/leetcode.java
+++ LEETCODE/leetcode.java
@@ -4,22 +4,50 @@
     
 // }
 
-leetcode 704 binary search
+// leetcode 704 binary search
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int slow=0;
+//         int fast=nums.length-1;
+//         while(slow<=fast){
+//         int mid=(slow+fast)/2;
+//             if(target>nums[mid]){
+//                 slow=mid+1;
+//             }
+//             else if(target==nums[mid]) return mid;
+//             else{
+//                 fast=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
+
+Solution 2 (using recursion)
+
 
 class Solution {
     public int search(int[] nums, int target) {
-        int slow=0;
-        int fast=nums.length-1;
-        while(slow<=fast){
-        int mid=(slow+fast)/2;
-            if(target>nums[mid]){
-                slow=mid+1;
-            }
-            else if(target==nums[mid]) return mid;
-            else{
-                fast=mid-1;
-            }
-        }
-        return -1;
+        int s=0;
+        int f=nums.length-1;
+        return search(nums,target,s,f);
+    }
+
+    public static int search(int[] arr, int t, int s, int f){
+        if (s > f) return -1;
+        int mid = s + (f - s) / 2;
+
+
+        if(t== arr[mid]) return mid;
+
+        if(t<arr[mid]) return search(arr, t, s, mid-1);
+
+        else return search(arr, t,mid+1, f);
+
     }
 }
