75b93163e0475cf854090f753d7409d1a674e8b1
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-08-07 18:30:27 +0530

    push 17

---
+++ RECURSION/leetcode.java
@@ -0,0 +1,163 @@
+
+import java.util.*;
+public class leetcode {
+    
+
+                            // leetcode 704 binary search
+
+// class Solution {
+    //     public int search(int[] nums, int target) {
+//         int slow=0;
+//         int fast=nums.length-1;
+//         while(slow<=fast){
+//         int mid=(slow+fast)/2;
+//             if(target>nums[mid]){
+    //                 slow=mid+1;
+    //             }
+//             else if(target==nums[mid]) return mid;
+//             else{
+//                 fast=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
+
+// Solution 2 (using recursion)
+
+
+// class Solution {
+    //     public int search(int[] nums, int target) {
+        //         int s=0;
+        //         int f=nums.length-1;
+        //         return search(nums,target,s,f);
+        //     }
+        
+        //     public static int search(int[] arr, int t, int s, int f){
+            //         if (s > f) return -1;
+            //         int mid = s + (f - s) / 2;
+            
+            
+            //         if(t== arr[mid]) return mid;
+            
+            //         if(t<arr[mid]) return search(arr, t, s, mid-1);
+            
+            //         else return search(arr, t,mid+1, f);
+            
+            //     }
+            // }
+            
+            
+            
+                            // factorial of a number using recursion
+
+
+    // class Solution {
+//     static int factorial(int n) {
+//     if(n<=1) return 1;
+//     return n*factorial(n-1);
+        
+//     }
+// }
+
+
+
+
+                            // Sum Of Digits
+
+
+// class Solution {
+    //     static int sumOfDigits(int n) {
+//         
+//         int digit=n%10;
+//         if(n==0) return 0;
+
+//         return digit+sumOfDigits(n/10);
+
+
+
+//     }
+// }
+
+
+
+
+                            //  REVERSE A NUMBER
+
+
+// static int sum =0;
+// public static int reverse(int n){
+//     if(n==0) return 0;
+//     int digit=n%10;
+//     sum=sum*10+digit;
+//     return reverse(n/10);
+// }
+// public static void main(String[] args) {
+//     Scanner s=new Scanner(System.in);
+//     int n=s.nextInt();
+//     reverse(n);
+//     System.out.println(sum);
+// }
+
+//  OR
+
+//  class Solution {
+//     public static int helper(int x, int rev){
+//         if(x == 0) return rev;
+//         if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10) return 0;
+//         rev = rev*10 + x % 10;
+//         return helper(x/10, rev);
+//     }
+//     public int reverse(int x) {
+//         return helper(x, 0);
+//     }
+// }
+
+
+
+                            //  PALINDROME NUMBER
+
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         int r=reverse(x,0);
+//        if(x<0){
+//              return false;
+//        }
+//         else if(x==r){
+//              return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+//     public int reverse(int x,int sum){
+//         if(x==0) return sum;
+//         int digit=x%10;
+//         sum=sum*10+digit;
+//         return reverse(x/10,sum);
+//     }
+// }
+
+
+                            //   NUMBER OF STEPS
+
+
+//     public static void steps(int n){
+//         if(n==0) return ;
+//         if(n%2==0){
+//             n=n/2;
+//             steps(n);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         int n=s.nextInt();
+//     }
+
+
+}
