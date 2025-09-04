425ced8cf51c935c2a8338ae9451becfcff6c516
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-09-04 12:02:30 +0530

    push 24

--- Array/leetcode.java
+++ Array/leetcode.java
@@ -133,3 +133,30 @@
         
 //     }
 // }
+
+
+
+
+
+
+//     744. Find Smallest Letter Greater Than Target
+
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         int s=0;
+//         int e=letters.length-1;
+//         int ans=0;
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+
+//             if(letters[mid] > target){
+//                 ans=mid;
+//                 e=mid-1;
+//             }
+//             else{
+//                 s=mid+1;
+//             }
+//         }
+//         return letters[ans];
+//     }
+// }
