753f2eaac940b3743d129d1dc78c43ac3e72883a
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-09-04 11:24:49 +0530

    push 23

--- Array/GFG.java
+++ Array/GFG.java
@@ -52,3 +52,37 @@
         
 //     }
 // }
+
+
+
+
+
+
+
+
+                // FLOOR OF A SORTED ARRAY
+
+// class Solution {
+//     public int findFloor(int[] arr, int x) {
+//         int s=0;
+//         int e=arr.length-1;
+//         int ans=-1;
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+//             if(x>=arr[mid]){
+//                 ans=mid;
+//                 s=mid+1;
+//             }
+//             else{
+//                 e=mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
