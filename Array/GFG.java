cf9713a9cf4ef5021bef501026a212aa66894ce7
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-09-06 21:38:05 +0530

    push 26

--- Array/GFG.java
+++ Array/GFG.java
@@ -1,23 +1,27 @@
 // package Array;
 
-// public class GFG {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import Array.array;
+
+public class GFG {
   
-// }
-
-
-
-//  element in array
-
-// class Solution {
+    
+    
+    
+    //  element in array
+    
+    // class Solution {
 //     public static int largest(int[] arr) {
-//         int slow=0;
-//         int fast=1;
+    //         int slow=0;
+    //         int fast=1;
 //         int max= Integer.MIN_VALUE;
 //         while(fast<arr.length){
 //             if(arr[slow]>max) max=arr[slow];
 //             if(arr[fast]>max) max= arr[fast];
             
-            
+
 //             slow++;
 //          Largest   fast++;
 //         }
@@ -30,25 +34,25 @@
 
 
 
-                //CEIL OF A SORTED ARRAY 
+//CEIL OF A SORTED ARRAY 
 
 // class Solution {
-//     public int findCeil(int[] arr, int x) {
-//         int s=0;
-//         int e=arr.length-1;
-//         int ans=-1;
-//         while(s<=e){
-//         int mid=s+(e-s)/2;
-        
-//         if(x>arr[mid]){
-//             s=mid+1;
-//         }
-//         else{
-//             ans=mid;
-//             e=mid-1;
-//         }
-//         }
-//         return ans;
+    //     public int findCeil(int[] arr, int x) {
+        //         int s=0;
+        //         int e=arr.length-1;
+        //         int ans=-1;
+        //         while(s<=e){
+            //         int mid=s+(e-s)/2;
+            
+            //         if(x>arr[mid]){
+                //             s=mid+1;
+                //         }
+                //         else{
+                    //             ans=mid;
+                    //             e=mid-1;
+                    //         }
+                    //         }
+                    //         return ans;
         
 //     }
 // }
@@ -60,22 +64,22 @@
 
 
 
-                // FLOOR OF A SORTED ARRAY
+// FLOOR OF A SORTED ARRAY
 
 // class Solution {
-//     public int findFloor(int[] arr, int x) {
-//         int s=0;
-//         int e=arr.length-1;
-//         int ans=-1;
-//         while(s<=e){
-//             int mid=s+(e-s)/2;
-//             if(x>=arr[mid]){
-//                 ans=mid;
-//                 s=mid+1;
-//             }
-//             else{
-//                 e=mid-1;
-//             }
+    //     public int findFloor(int[] arr, int x) {
+        //         int s=0;
+        //         int e=arr.length-1;
+        //         int ans=-1;
+        //         while(s<=e){
+            //             int mid=s+(e-s)/2;
+            //             if(x>=arr[mid]){
+                //                 ans=mid;
+                //                 s=mid+1;
+                //             }
+                //             else{
+                    //                 e=mid-1;
+                    //             }
 //         }
 //         return ans;
 //     }
@@ -86,3 +90,42 @@
 
 
 
+
+
+//      Find position of an element in a sorted array of infinite numbers
+
+
+public static int idx(int[] arr,int k){
+        int s=0;
+        int e=1;
+
+        while(e<arr.length && arr[e]<k){
+            int temp=e+1;
+            e=e+(e-s+1)*2;
+            s=temp;
+        }
+        return find(arr, k, s, e);
+}
+public static int find(int[] arr, int k,int s,int e){
+    while(s<=e){
+        int mid=s+(e-s)/2;
+
+        if(k==arr[mid])  return mid;
+
+        else if(k>arr[mid]) s=mid+1;
+
+        else{ e=mid-1;}
+    }
+    return -1;
+    
+}
+public static void main(String[] args) {
+    int[] arr={12,33,42,55,67,84,90,150,163,170,211};
+    int k = 170;
+    int ans=idx(arr, k);
+    System.out.println(ans);
+}
+
+
+
+}
