58d4b979dd5fa34f009b360c03a7c77aa04a6e68
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-09-09 23:08:55 +0530

    push 29

--- Array/leetcode.java
+++ Array/leetcode.java
@@ -304,3 +304,85 @@
 //         return -1;
 //     }
 // }
+
+
+
+
+//      33. Search in Rotated Sorted Array
+
+            //  Solution 1
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int left = 0;
+//         int right = nums.length - 1;
+
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] >= nums[left]) {
+//                 if (nums[left] <= target && target <= nums[mid]) {
+//                     right = mid - 1;
+//                 } else {
+//                     left = mid + 1;
+//                 }
+//             } else {
+//                 if (nums[mid] <= target && target <= nums[right]) {
+//                     left = mid + 1;
+//                 } else {
+//                     right = mid - 1;
+//                 }
+//             }
+//         }
+
+//         return -1;        
+//     }
+// }
+
+
+//        SOLUTION 2
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//       int pivot=findpivot(nums);
+//       if(pivot==-1) return find(nums,target,0,nums.length-1);
+//       if(nums[pivot]==target) return pivot;
+//       int first=find(nums,target,0,pivot);
+//       if(first != -1) return first;
+//       else return find(nums,target,pivot+1,nums.length-1);
+//     }
+
+//     public int findpivot(int[] nums){
+//        int s=0;
+//        int e=nums.length-1;
+//         if (nums[s] <= nums[e]) return -1;
+
+//        while(s<e){
+//         int mid=s+(e-s)/2;
+//         if(nums[mid]>nums[mid+1] && mid<e) return mid;
+//         else if(nums[mid]<nums[mid-1] && mid>s) return mid-1;
+//         else{
+//             if (nums[mid] >= nums[s]) {
+//                 s = mid + 1;
+//             } else {
+//                 e = mid - 1;
+//             }
+//         }
+//        }
+//        return -1;
+//     }
+
+//     public int find(int[] nums, int target,int s,int e){
+//         while(s<=e){
+//         int mid=s+(e-s)/2;
+//         if(nums[mid]>target) e=mid-1;
+//         else if(nums[mid]< target) s=mid+1;
+//         else{
+//             return mid;
+//         }
+//        }
+//        return -1;
+//     }
+//     }
